d5f19d783e26a7c782afa221a012ab1e567ba39c
aaronburmeister <f34r91@gmail.com>
2020-04-28 13:26:10 -0600

    Created DB and migrated Users table

--- db/migrate/20200428163918_devise_create_users.rb
+++ db/migrate/20200428163918_devise_create_users.rb
@@ -32,6 +32,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      # Additional attributes
+      t.string :username, null: false
+      t.boolean :is_artist
+
       t.timestamps null: false
     end
 
