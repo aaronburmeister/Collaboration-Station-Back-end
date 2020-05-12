1267cc4846a267f373755f89f9d7aa67b955c6c5
aaronburmeister <f34r91@gmail.com>
2020-05-12 14:09:54 -0600

    Implemented Auth Using JWT tokens

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
   scope :api, defaults: { format: :json } do
-    devise_for :users
+    devise_for :users, controllers: { sessions: :sessions },
+                       path_names: { sign_in: :login }
     resources :request_comments
     resources :requests
     resources :image_comments
     resources :images
+    resources :users
   end
 end
