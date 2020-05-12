4318fc69221ec1bb3ead2ee7042576900f6712e1
aaronburmeister <f34r91@gmail.com>
2020-05-12 14:59:13 -0600

    Troubleshooting authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API  
+class ApplicationController < ActionController::Base 
     protect_from_forgery with: :null_session
 
     respond_to :json
@@ -16,13 +16,13 @@ class ApplicationController < ActionController::API
     def authenticate_user
         if request.headers['Authorization'].present?
             authenticate_or_request_with_http_token do |token|
-            begin
-                jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+                begin
+                    jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
 
-                @current_user_id = jwt_payload['id']
-            rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
-                head :unauthorized
-            end
+                    @current_user_id = jwt_payload['id']
+                rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+                    head :unauthorized
+                end
             end
         end
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  protected
   
   def generate_jwt
     JWT.encode({ id: id,
                 exp: 60.days.from_now.to_i },
                 Rails.application.secrets.secret_key_base)
   end
+
 end
